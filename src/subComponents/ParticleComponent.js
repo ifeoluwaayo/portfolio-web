3fa838bb0fe02d8fef4510cfe9ebea544dfbfb05
ifeoluwaayo <adeayomide112@gmail.com>
2024-04-21 13:16:11 -0700

    okay

--- src/subComponents/ParticleComponent.js
+++ src/subComponents/ParticleComponent.js
@@ -1,29 +1,29 @@
 import React from "react";
-import Particles from "react-particles-js";
 import styled from "styled-components";
-// tsparticles
+// import Particles from "react-tsparticles";
+// // tsparticles
 
-//particle config files
+// //particle config files
 import configDark from "../config/particlesjs-config.json";
 import configLight from "../config/particlesjs-config-light.json";
 
 const Box = styled.div`
-	position: absolute;
-	top: 0;
-	right: 0;
-	left: 0;
-	bottom: 0;
-	z-index: 0;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 0;
 `;
 const ParticleComponent = (props) => {
-	return (
-		<Box>
-			<Particles
-				style={{ position: "absolute", top: 0 }}
-				params={props.theme === "light" ? configLight : configDark}
-			/>
-		</Box>
-	);
+  return (
+    <Box>
+      {/* <Particles
+        style={{ position: "absolute", top: 0 }}
+        params={props.theme === "light" ? configLight : configDark}
+      /> */}
+    </Box>
+  );
 };
 
 export default ParticleComponent;
